57ebaac00d075ce1d0cb479bcd404193c4cb2bdd
Shimon <sh1m_@hotmail.com>
2019-01-16 15:42:17 +0200

    Добавлены роли по-умолчанию. Стили для страницы изменения инфо прользователя.

---
+++ AspPlanApp/Data/RoleInit.cs
@@ -0,0 +1,55 @@
+﻿using AspPlanApp.Models.DbModels;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspPlanApp.Data
+{
+    public class RoleInit
+    {
+
+        public static void RoleInitAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string adminName = "admin";
+            string adminMail = "admin@mail.com";
+            string adminPassword = "admin";
+
+            if (roleManager.FindByNameAsync("admin").Result == null)
+            {
+                roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+
+            if (roleManager.FindByNameAsync("owner").Result == null)
+            {
+                roleManager.CreateAsync(new IdentityRole("owner"));
+            }
+
+            if (roleManager.FindByNameAsync("staff").Result == null)
+            {
+                roleManager.CreateAsync(new IdentityRole("staff"));
+            }
+
+            if (roleManager.FindByNameAsync("user").Result == null)
+            {
+                roleManager.CreateAsync(new IdentityRole("user"));
+            }
+
+            if (userManager.FindByEmailAsync(adminMail).Result == null)
+            {
+                User adminUser = new User() { UserName = adminName, Email = adminMail };
+
+                var resultTask = userManager.CreateAsync(adminUser, adminPassword);
+                IdentityResult result = resultTask.Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(adminUser, "admin");
+                }
+            }
+
+        }
+
+    }
+}
--- AspPlanApp/Program.cs
+++ AspPlanApp/Program.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using AspPlanApp.Data;
+using AspPlanApp.Models.DbModels;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace AspPlanApp
@@ -14,11 +18,34 @@ namespace AspPlanApp
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            //CreateWebHostBuilder(args).Build().Run();
+
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var userManager = services.GetRequiredService<UserManager<User>>();
+                    var rolesManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    RoleInit.RoleInitAsync(userManager, rolesManager);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build();
     }
 }
